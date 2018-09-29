0b2f2f74282009f623355c71a2e6177765173bf7
Jacob Niedzwiecki <jacob@jakemoves.com>
2018-09-29 10:33:15 -0400

    Initial Commit

---
+++ Cohort-iOS/Cohort_iOS.h
@@ -0,0 +1,19 @@
+//
+//  Cohort_iOS.h
+//  Cohort-iOS
+//
+//  Created by Jacob Niedzwiecki on 2018-09-29.
+//  Copyright © 2018 Jacob Niedzwiecki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cohort_iOS.
+FOUNDATION_EXPORT double Cohort_iOSVersionNumber;
+
+//! Project version string for Cohort_iOS.
+FOUNDATION_EXPORT const unsigned char Cohort_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cohort_iOS/PublicHeader.h>
+
+
