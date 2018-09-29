af1af71bddc03c5cf1ac7e003c1c498317bc86fe
Jacob Niedzwiecki <jacob@jakemoves.com>
2018-09-29 15:31:13 -0400

    Added call to register for notifications

--- Sources/Cohort-iOS/CHSession.swift
+++ Sources/Cohort-iOS/CHSession.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 import Starscream
+import Alamofire
 
 public class CHSession: NSObject{
-    private let socketURL: URL
+    private let serverURL: URL
+    
     private var socket: WebSocket
     
     public var isConnected: Bool {
         get { return socket.isConnected }
     }
     
+    private var httpURL: URL {
+        get { return serverURL.appendingPathComponent(":3000")}
+    }
+    
+    private var socketURL: URL {
+        get { return serverURL.appendingPathComponent(":8080")}
+    }
+    
     public init(cohortServerURL url: URL){
-        socketURL = url
-        socket = WebSocket(url: self.socketURL)
+        serverURL = url
+        
+        socket = WebSocket(url: serverURL.appendingPathComponent(":8080"))
         
         super.init()
         
@@ -30,6 +41,13 @@ public class CHSession: NSObject{
         socket.connect()
         while (!isConnected) {}
         return completion(true)
+    }
+    
+    public func registerForNotifications(token: Data, completion: (_ result: Bool) -> ()){
+        let tokenString = token.hexString()
+        debugPrint(tokenString)
+        let params: Parameters = [ "token": tokenString ]
+        Alamofire.request(serverURL.absoluteString + "/register-for-notifications", method: .post, parameters: params, encoding: JSONEncoding.default).validate()
     }
 }
 
@@ -48,5 +66,16 @@ extension CHSession: WebSocketDelegate {
     
     public func websocketDidReceiveData(socket: WebSocketClient, data: Data) {
         
+    }
+}
+
+extension Data {
+    func hexString() -> String {
+        var bytesPointer: UnsafeBufferPointer<UInt8> = UnsafeBufferPointer(start: nil, count: 0)
+        self.withUnsafeBytes { (bytes) in
+            bytesPointer = UnsafeBufferPointer<UInt8>(start: UnsafePointer(bytes), count:self.count)
+        }
+        let hexBytes = bytesPointer.map { return String(format: "%02hhx", $0) }
+        return hexBytes.joined()
     }
 }
--- examples/ios-demo/AppDelegate.swift
+++ examples/ios-demo/AppDelegate.swift
@@ -8,22 +8,61 @@
 
 import UIKit
 import Cohort_iOS
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var userNotificationCenter: UNUserNotificationCenter?
+    
     var cohortSession: CHSession?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         cohortSession = CHSession(cohortServerURL: URL(string: "http://jakemoves.local:8080")!)
+        cohortSession?.connect(completion: { (result) in
+            if !result {
+                debugPrint("failed to connect to cohort server")
+            }
+        })
+        
+        userNotificationCenter = UNUserNotificationCenter.current()
+        
+        let opts: UNAuthorizationOptions = [.alert, .sound, .badge]
+        
+        userNotificationCenter?.requestAuthorization(options: opts, completionHandler: { (result, error) in
+            if !result {
+                debugPrint(error)
+            }
+        })
+        
+//        // to check after app startup
+//        center.getNotificationSettings { (settings) in
+//            if settings.authorizationStatus != .authorized {
+//                // Notifications not allowed
+//            }
+//        }
+        
+        application.registerForRemoteNotifications()
         
         return true
     }
 
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data){
+        debugPrint("token: \(deviceToken)")
+        self.cohortSession?.registerForNotifications(token: deviceToken, completion: { (result) in
+            if !result {
+                debugPrint("Error registering for notifications")
+            }
+        })
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        debugPrint(error)
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
